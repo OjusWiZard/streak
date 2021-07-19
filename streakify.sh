64a981a5a9cee8f468b0000021df3a6335f83cbb
OjusWiZard <ojuswimail@gmail.com>
2021-07-19 20:03:05 +0530

    update streak for

---
+++ streakify.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+STREAKFILE='.streak'
+
+DATE=$1
+
+if [[ -z $DATE ]]
+then
+    DATE=`date`
+else
+    DATE=`date -d "$DATE 13:33:33"`
+fi
+
+echo 'Keep that streak going... artificially'
+
+echo `date -Iseconds -d "$DATE"` >> $STREAKFILE
+git commit -a -m "update streak for `date -I -d "$DATE"`" --date="$DATE"
+git push
+
+echo 'Congratulations, you just kept your streak going'
